7c4c5369a5f590d6e1b56e32fc13af73b332cf14
bazigan <bazigannetwork@gmail.com>
2024-08-22 22:45:42 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/api', (req, res) => {
     res.json({ message: "Hello from Backend!" });
 });
